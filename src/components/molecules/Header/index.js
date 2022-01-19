d529a71c3028a403757ebf2785877401cd36b3ac
Ray Yam <ryamhkdev@gmail.com>
2022-01-20 00:47:31 +0800

    add theme switch

--- src/components/molecules/Header/index.js
+++ src/components/molecules/Header/index.js
@@ -1,27 +1,45 @@
 import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
+import useSettings from '../../../hooks/useSettings';
+import useDatabase from '../../../hooks/useDatabase';
+import useSnackbar from '../../../hooks/useSnackbar';
 import useStyles from '../../../hooks/useStyles';
 import styles from './Header.module.css';
 
 import Icon from '../../atoms/Icon';
 import Button from '../../atoms/Button';
-
+import Switch from '../../atoms/Switch';
 
 export default function Header(props) {
   const {
-    actions,
     headline,
     hidden = false,
   } = props;
 
   const router = useRouter();
+  const db = useDatabase('my-test-app');
+  const [settings, reloadSettings] = useSettings();
+  const [setSnackbar] = useSnackbar();
   const css = useStyles(styles);
 
-  if (hidden) {
+  if (hidden || !settings) {
     return null;
   }
 
   const redirectPrevPage = () => router.back();
+  const onChange = async () => {
+    try {
+      const store = await db.connect('common', 'readwrite');
+      const value = {
+        mode: settings.theme.mode === 'dark' ? 'light' : 'dark',
+        color: settings.theme.color,
+      };
+      await store.put({ id: 'theme', value });
+      reloadSettings('theme');
+    } catch ({ name, message }) {
+      setSnackbar('error', `${name}: ${message}`);
+    }
+  };
 
   return (
     <header className={css('header')}>
@@ -33,13 +51,18 @@ export default function Header(props) {
         <Icon icon="arrow_back" />
       </Button>
       <h1 className={css('headline')}>{headline}</h1>
-      {actions}
+      <Switch
+        className={css('switch')}
+        checked={settings.theme.mode === 'dark'}
+        checkedIcon={<Icon icon="dark_mode" className={css('icon', 'no-select')} />}
+        uncheckedIcon={<Icon icon="light_mode" className={css('icon', 'no-select')} />}
+        onChange={onChange}
+      />
     </header>
   )
 }
 
 Header.propTypes = {
-  actions: PropTypes.node,
   headline: PropTypes.string,
   hidden: PropTypes.bool,
 };
